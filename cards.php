abc120cc3aaa32bdcfac1d47edf4059a5b1ed2d0
Noha <noha.ben-salem@plateforme.io>
2024-10-14 11:41:48 +0200

    Envoi Noms images

--- cards.php
+++ cards.php
@@ -4,6 +4,17 @@ class Cards{
     protected $contenu;
 }
 
-// $image1 = 
+$image1 = "images/number_one.png"; 
+$image2 = "images/number_two.png"; 
+$image3 = "images/number_three.png"; 
+$image4 = "images/number_four.png"; 
+$image5 = "images/number_five.png"; 
+$image6 = "images/number_six.png"; 
+$image7 = "images/number_seven.png"; 
+$image8 = "images/number_eight.png"; 
+$image9 = "images/number_nine.png"; 
+$image10 = "images/number_ten.png"; 
+$image11 = "images/number_eleven.png";
+$image12 = "images/number_twelve.png"; 
 ?>
 
