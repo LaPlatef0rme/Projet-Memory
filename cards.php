1f08f006553df521bd5fb004959e7b90966cc68f
Noha <noha.ben-salem@plateforme.io>
2024-10-14 16:23:16 +0200

    Envoi Images Nombres

--- cards.php
+++ cards.php
@@ -1,8 +1,28 @@
 <?php
-class Cards{
+class Cards {
     protected $id;
     protected $contenu;
+
+    public function __construct($id, $contenu) {
+        $this->id = $id;
+        $this->contenu = $contenu;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getContenu() {
+        return $this->contenu;
 }
+}
+
+// Définir la variable avant de l'utiliser
+$image1 = "images/number_one.png";
+
+// Utiliser correctement la variable dans l'echo
+echo '<img src="' . $image1 . '" alt="Description de l\'image">';
+
 
 $image1 = "images/number_one.png"; 
 $image2 = "images/number_two.png"; 
@@ -18,3 +38,16 @@ $image11 = "images/number_eleven.png";
 $image12 = "images/number_twelve.png"; 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+</body>
+</html>
